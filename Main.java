c498da52aca50c71cf669188684dfa8439cb1f8e
harun2301 <49957931+harun2301@users.noreply.github.com>
2021-10-28 06:00:19 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+/**
+* @author Oscar Baños
+*/
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("La suma es:" + suma(2,2));
+  }
+
+  /**
+  * @param  a Primer entero de la función
+  * @param  b Segunfo entero de la función
+  * @return La suma de los enteros a y b
+  */
+  public static int suma(int a, int b){
+    return a + b;
+  }
+
+}
